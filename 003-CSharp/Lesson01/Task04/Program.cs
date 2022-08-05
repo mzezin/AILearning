6c8dcdbf9016fd9bfd6426e44e9cf7b79fc27407
Maksim Zezin <mzezin@gmail.com>
2022-08-05 23:19:43 +0300

    c# seminar 1 homework

---
+++ 003-CSharp/Lesson01/Task04/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Поочередно введите 3 числа:");
+int numberA = int.Parse(Console.ReadLine());
+int numberB = int.Parse(Console.ReadLine());
+int numberC = int.Parse(Console.ReadLine());
+int max = numberA > numberB ? numberA : numberB;
+max = max > numberC ? max : numberC;
+Console.WriteLine($"Большее число: {max}");
---
+++ 003-CSharp/Lesson01/Task08/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+
+Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine());
+int iterator = 2;
+List<int> result =  new List<int>();
+while (iterator <= number)
+{
+  result.Add(iterator);
+  iterator += 2;
+}
+Console.WriteLine(string.Join(", ", result));
