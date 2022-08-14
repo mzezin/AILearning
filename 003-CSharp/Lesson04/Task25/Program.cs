42ac9e08acd0fd41ef4ed4e2e318c66f105bd3b9
Maksim Zezin <mzezin@gmail.com>
2022-08-14 15:39:20 +0300

    Seminar 4 homework

---
+++ 003-CSharp/Lesson04/Task25/Program.cs
@@ -0,0 +1,15 @@
+﻿//  Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+double myPow( double a, int b) {
+  double result = 1;
+  for (int i = 1; i <= b; i++)
+  {
+    result = result * a;
+  }
+  return result;
+}
+
+Console.WriteLine(myPow(3, 5));
+Console.WriteLine(myPow(2, 4));
---
+++ 003-CSharp/Lesson04/Task27/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int digitsSum(int number)
+{
+    return number.ToString().ToCharArray().Select(x => int.Parse(x.ToString())).Sum();
+}
+;
+
+string input = Console.ReadLine();
+
+if (int.TryParse(input, out int number))
+{
+    Console.WriteLine(digitsSum(number));
+}
+else
+{
+    Console.WriteLine("Введите целое число");
+}
+;
