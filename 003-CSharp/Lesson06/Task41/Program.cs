cb93352467d4d0eab919c1fd0b0efeb761f0056d
Maksim Zezin <mzezin@gmail.com>
2022-08-22 14:03:44 +0300

    Seminar 6 homework

---
+++ 003-CSharp/Lesson06/Task41/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+int countPositives(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+            result++;
+    }
+    return result;
+}
+
+string prettyPrintArray(int[] array)
+{
+    return ($"[{String.Join(", ", array)}]");
+}
+
+Console.WriteLine("Введите количество чисел:");
+if (!int.TryParse(Console.ReadLine(), out int m))
+    throw new Exception("Неправильный ввод");
+int[] array = new int[m];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine($"Введите число № {i + 1}: ");
+    if (int.TryParse(Console.ReadLine(), out int input))
+    {
+        array[i] = input;
+    }
+    else
+    {
+        Console.WriteLine("Неправильный ввод, введите число повторно");
+        i--;
+    }
+}
+
+Console.WriteLine($"Исходный массив: {prettyPrintArray(array)}");
+Console.WriteLine($"Количество чисел больше нуля: {countPositives(array)}");
---
+++ 003-CSharp/Lesson06/Task43/Program.cs
@@ -0,0 +1,76 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// основное решение
+double[]? intersectionPoint(int b1, int k1, int b2, int k2)
+{
+    if (k1 == k2)
+        return null;
+    double x = 1.0 * (b1 - b2) / (k2 - k1);
+    double y = 1.0 * (k2 * b1 - k1 * b2) / (k2 - k1);
+    return new double[2] { x, y };
+}
+
+double[]? r = intersectionPoint(2, 5, 4, 9);
+double[]? r = intersectionPoint(0, 1, 1, 0);
+double[]? r = intersectionPoint(1, 1, 1, 1);
+
+if (r != null)
+    Console.WriteLine($"Точка пересечения прямых: ({r[0]}; {r[1]})");
+else
+    Console.WriteLine("Прямые не пересекаются или совпадают");
+
+//==================================================================
+// развлечения с классами
+
+Line lineA = new Line(2, 5);
+Line lineB = new Line(4, 9);
+Point? p = lineA.FindIntersection(lineB);
+
+if (p != null)
+{
+    Console.Write("Точка пересечения прямых: ");
+    p.PrintPoint();
+}
+else
+    Console.WriteLine("Прямые не пересекаются или совпадают");
+
+class Point
+{
+    public double X { get; }
+    public double Y { get; }
+
+    public Point(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public void PrintPoint()
+    {
+        Console.WriteLine($"({X}; {Y})");
+    }
+}
+
+class Line
+{
+    public double K { get; }
+    public double B { get; }
+
+    public Line(double b, double k)
+    {
+        K = k;
+        B = b;
+    }
+
+    public Point? FindIntersection(Line line)
+    {
+        if (K == line.K)
+            return null;
+        double x = 1.0 * (B - line.B) / (line.K - K);
+        double y = 1.0 * (line.K * B - K * line.B) / (line.K - K);
+        return new Point(x, y);
+    }
+}
